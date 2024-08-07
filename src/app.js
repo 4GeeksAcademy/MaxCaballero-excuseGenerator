6b32ed84bd8b7ed1abbfa26540f96039dc8b3ea2
Christian_Caballero <94983984+xtiancr78@users.noreply.github.com>
2024-08-07 22:09:52 +0000

    termine la funcionalidad del excuse generator y  se renderiza en el index.html

--- src/app.js
+++ src/app.js
@@ -7,5 +7,38 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function excuseme() {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let whoxAzar = Math.floor(Math.random() * who.length);
+    let valorWho = who[whoxAzar];
+
+    let action = ["ate", "peed", "crushed", "broke"];
+    let actionAzar = Math.floor(Math.random() * action.length);
+    let valorAction = action[actionAzar];
+
+    let what = ["my homework", "the keys", "the car"];
+    let whatAzar = Math.floor(Math.random() * what.length);
+    let valorWhat = what[whatAzar];
+
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+    let whenAzar = Math.floor(Math.random() * when.length);
+    let valorWhen = when[whenAzar];
+
+    let excusas =
+      valorWho + " " + valorAction + " " + valorWhat + " " + valorWhen;
+    return excusas;
+  }
+
+  let excusass = excuseme();
+
+  document.getElementById("excuse").innerHTML = excusass;
+
+  console.log(excusass);
 };
